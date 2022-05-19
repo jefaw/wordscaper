ccd056483420a9add109b6899fd7139e9b97e617
Jeffrey Wang <jwan2369@uwo.ca>
2022-05-19 17:02:21 -0400

    input changes api request. moved stuff around for better organization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import FetchWords from './components/FetchWords'
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+  
+  const [words, setWords] = useState(["none"]);
+  const [userInput, setUserInput] = useState('');
+
+  const newRequest = async() =>{ // after first render
+    setWords("Requesting...");
+    //const url = "https://api.datamuse.com/words?ml=spoon&sp=*a&max=10";
+    const url = "https://api.datamuse.com/words?sp=//" + userInput;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    let w = []
+    for(let i=0;i<data.length;i++){
+        w[i] = data[i]['word'];
+        console.log(i + data[i]['word']);
+    }
+    setWords(w);
+  };
+  const changeInput = (newInput) => {
+    setUserInput(newInput)
+  };
+  
+  return (    
     <div className="App">
       <header className="App-header">
         <h1> Wordscaper</h1>
@@ -11,11 +34,8 @@ function App() {
         <button>5 letter</button>
         <button>6 letter</button>
         <button>7 letter</button>
-        <form>
-        <input type="text" placeholder='Search letters here'></input>
-        <input type="submit"></input>
-        </form>
-        <FetchWords />
+        <FetchWords onSubmit={newRequest} words={words} onChange={changeInput}/>
+        
         
       </header>
     </div>
--- src/components/FetchWords.js
+++ src/components/FetchWords.js
@@ -1,27 +1,30 @@
 import React from "react";
 
 export default class FetchWords extends React.Component{
-    state = {
-        loading: true,
-        words: [],
-    };
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
     
-    async componentDidMount(){ // after first render
-        const url = "https://api.datamuse.com/words?ml=spoon&sp=*a&max=10";
-        const response = await fetch(url);
-        const data = await response.json();
-        let w = []
-        for(let i=0;i<data.length;i++){
-            w[i] = data[i]['word'];
-            console.log(i + data[i]['word']);
-        }
-        this.setState({words: w});
+    handleSubmit(e){
+        this.props.onSubmit();
 
     }
+    handleChange(e){
+        const input = e.target.value;
+        this.props.onChange(input)
+    }
+    //{this.props.words.map(word => <div>{word}</div>)}
     render(){
         return (<div>
+            <input type="text" placeholder='Search letters here' onChange={this.handleChange}></input>
+            <button onClick={this.handleSubmit}>Submit</button>
+            
             <h3>Potential Solutions:</h3>
-            {this.state.words.map(word => <div>{word}</div>)}
+            {Array.from(this.props.words).map(word => <li>{word}</li>)}
+
+            
         </div>
         );
     }
